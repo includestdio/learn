66b24fe0db618aa5f2891a0a44ebbb9c1a24a910
fly <onemedusa@163.com>
2020-05-04 23:36:01 +0800

    fly：自定义注解新增文件提交

---
+++ Java/Java_L2/第一阶段/Spring/code/lagou-transfer/src/main/java/com/lagou/edu/config/MyAutowired.java
@@ -0,0 +1,10 @@
+package com.lagou.edu.config;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyAutowired {
+    String value() default "";
+}
---
+++ Java/Java_L2/第一阶段/Spring/code/lagou-transfer/src/main/java/com/lagou/edu/utils/MyApplicationContext.java
@@ -0,0 +1,16 @@
+package com.lagou.edu.utils;
+
+
+public class MyApplicationContext {
+
+    private String packageName;
+
+    public MyApplicationContext(String packageName){
+        this.packageName = packageName;
+    }
+
+    public void initBeans(){
+
+    }
+
+}
