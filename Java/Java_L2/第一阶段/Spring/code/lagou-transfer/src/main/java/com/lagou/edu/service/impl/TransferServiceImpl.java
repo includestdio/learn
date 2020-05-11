412e62b829fb6517968357bc20d697060ccf5467
fly <onemedusa@163.com>
2020-05-11 23:28:57 +0800

    fly: 漏提代码

---
+++ Java/Java_L2/第一阶段/Spring/code/lagou-transfer/src/main/java/com/lagou/edu/service/impl/TransferServiceImpl.java
@@ -0,0 +1,43 @@
+package com.lagou.edu.service.impl;
+
+import com.lagou.edu.config.MyAutowired;
+import com.lagou.edu.config.MyService;
+import com.lagou.edu.dao.AccountDao;
+import com.lagou.edu.factory.BeanFactory;
+import com.lagou.edu.pojo.Account;
+import com.lagou.edu.service.TransferService;
+
+/**
+ * @author 应癫
+ */
+@MyService
+public class TransferServiceImpl {
+
+    //private AccountDao accountDao = new JdbcAccountDaoImpl();
+
+     private AccountDao accountDao = (AccountDao) BeanFactory.getBean("accountDao");
+
+    // 最佳状态
+//    private AccountDao accountDao;
+
+    // 构造函数传值/set方法传值
+
+//    public void setAccountDao(AccountDao accountDao) {
+//        this.accountDao = accountDao;
+//    }
+
+
+
+    public void transfer(String fromCardNo, String toCardNo, int money) throws Exception {
+
+            Account from = accountDao.queryAccountByCardNo(fromCardNo);
+            Account to = accountDao.queryAccountByCardNo(toCardNo);
+
+            from.setMoney(from.getMoney()-money);
+            to.setMoney(to.getMoney()+money);
+
+            accountDao.updateAccountByCardNo(to);
+//            int c = 1/0;
+            accountDao.updateAccountByCardNo(from);
+    }
+}
